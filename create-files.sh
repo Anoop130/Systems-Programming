fddb971274283718d96837b502bf1c6b9adc921a
Anoop130 <anoopmishra0130@gmail.com>
2025-03-12 13:07:16 -0500

    Adding directories

--- create-files.sh
+++ create-files.sh
@@ -1,13 +1,16 @@
 # list of file names
-files=("1.C++ vs C" "2.Reading and Writing Data 1" 
+folders=("1.C++ vs C" "2.Reading and Writing Data 1" 
 	"3.Reading and Writing Data 2" "4.Reading values at regular time intervals"
 	"5.A Timed Quiz" "6.Handling multiple signals from the child"
 	"7.The Hi-Lo Game implemented with Multiple Processes"
 	"8.Using exit, exec, and wait")
 
-# creating files for each file name
-for file in "${files[@]}"; do
-	mkdir "$file"
+# creating directories for each foldername
+for folder in "${folders[@]}"; do
+	
+	mkdir "$folder" # creating dir
+	cd "$folder"
+	touch "${folder:0:1}.gitkeep" # creating .gitkeep files for empty directory
+ 	cd ..
 done
-
 
